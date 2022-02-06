f898ba4f8ed0240c8cc4d5db964a17ad4d31120f
VladimirAlexeev <34220627+VladimirAlexeev@users.noreply.github.com>
2022-02-06 16:22:42 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,19 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { usersList } from './usersList';
 
+// create an express application
 const app = express();
 const PORT = 9000;
 
-//middleware
+// apply middleware to parse incoming requests as JSON
 app.use(bodyParser.json());
 
+// create route to handle HTTP GET request
 app.get("/", (_req, res) => {
     return res.send(usersList);
 });
 
+// create route to handle HTTP POST request
 app.post("/delete-user", (req, res) => {
     const id: number = req.body.id;
 
@@ -24,6 +27,7 @@ app.post("/delete-user", (req, res) => {
     return res.send("Failed to delete user!");
 });
 
+// create Node server at specified port
 app.listen(PORT);
 
 console.log(`[app]: Is running on http://localhost:${PORT}`);
